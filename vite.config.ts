86175cd9edc3039b614c3c86b6fae2a19fc94f31
Jazarumag <118693522+Jazarumag@users.noreply.github.com>
2025-07-14 15:05:33 +0000

    guia 17

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,48 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/dashboard",
-  plugins: [react()],
+  base: "/dashboard/",
+  plugins: [react(),
+  VitePWA({
+         registerType: 'autoUpdate',
+         devOptions: {
+            enabled: true
+         },
+         includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+         manifest: {
+            id: '/dashboard/',
+            name: 'Dashboard del Clima - Proyecto 04',
+            short_name: 'Dashboard del Clima',
+            description: 'Proyecto 04 - dashboard del clima desarrollado con React y MUI',
+            theme_color: '#D3D1D1',
+            icons: [
+              {
+                  src: 'pwa-192x192.png',
+                  sizes: '192x192',
+                  type: 'image/png',
+              },
+              {
+                  src: 'pwa-512x512.png',
+                  sizes: '512x512',
+                  type: 'image/png',
+              },
+              {
+                  src: "pwa-maskable-192x192.png",
+                  sizes: "192x192",
+                  type: "image/png",
+                  purpose: "maskable"
+              },
+              {
+                  src: "pwa-maskable-512x512.png",
+                  sizes: "512x512",
+                  type: "image/png",
+                  purpose: "maskable"
+              },
+            ]
+        }
+      })
+  ]
 })
